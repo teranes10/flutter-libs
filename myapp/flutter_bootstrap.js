8a824393eb3cc8fdff6fd4864636a8979873ea0c
teranes10 <teranes10@users.noreply.github.com>
2026-08-01 06:57:21 +0000

    deploy: dfc34683072090b09faed2d4f888295329d86cd4

--- myapp/flutter_bootstrap.js
+++ myapp/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1527ae0ec577a4ef50e65f6fefcfc1326707d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1979694869"
+    serviceWorkerVersion: "1244956080"
   }
 });
