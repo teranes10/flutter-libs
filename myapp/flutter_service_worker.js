0a32165eb6d5c555b893cef70e81719efa20fd20
teranes10 <teranes10@users.noreply.github.com>
2026-08-03 20:23:44 +0000

    deploy: 107a0af02d7fa61f282c65e9f60dbf6c897159f0

--- myapp/flutter_service_worker.js
+++ myapp/flutter_service_worker.js
@@ -4,7 +4,7 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"flutter_bootstrap.js": "97eed23135b578e99593746bfe31d254",
+"flutter_bootstrap.js": "a68a195b3180a300924e93b02a4bbb03",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
